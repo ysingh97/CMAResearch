a2f00d2d92722a9fd8e4e2a3a423a635d58de4f9
ysingh97 <bubstubs@gmail.com>
2019-03-25 03:28:50 -0400

    changed to wide foot simulator

--- CMA.py
+++ CMA.py
@@ -9,7 +9,7 @@ import time
 import math
 import os
 
-SIMULATOR = Creature_Simulators.LeftRightSymmetricCapsuleQuadSimulator
+SIMULATOR = Creature_Simulators.LeftRightSymmetricWideFootQuad
 general_option = {'maxiter': 20, 'popsize': 16}
 DURATION = 7
 
